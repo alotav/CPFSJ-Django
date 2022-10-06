4d24dc88af7790e53b16b887a283236ac795a18f
alotav <alonsotaverna@hotmail.com>
2022-10-06 18:24:45 -0300

    agregamos requirements txt para python anywhere

--- CPFSJ/settings.py
+++ CPFSJ/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yusklhuc5br$ev&gqku=ewx-i^&b+niwp1s9cie*v-#w-q0rr-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
