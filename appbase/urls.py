8945ef2f08bf5b3deb27e61d81d2ea7cc94a1480
alotav <alonsotaverna@hotmail.com>
2022-10-26 16:29:03 -0300

    modificacion de css y html gral

--- appbase/urls.py
+++ appbase/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # Pwd reset
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="autenticacion/reinicio_pwd.html"), name="password_reset"),
     path('reset_password_send/', auth_views.PasswordResetDoneView.as_view(template_name="autenticacion/reinicio_enviado.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="autenticacion/reinicio_confirmacion.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="autenticacion/reinicio_completo.html"), name="password_reset_complete"),
 ]
 
